e1fb2b5b1d82decdb306ea66a381005aa7e27982
Anmol Kaushik <73958690+anmol-ui@users.noreply.github.com>
2022-12-22 12:32:51 +0530

    Create Confirm.js

---
+++ src/components/Confirm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Confirm(){
+    return (
+        <div>
+            <p>
+                Email sent! Check your inbox to confirm email address
+            </p>
+        </div>
+    );
+}
+
+export default Confirm;
