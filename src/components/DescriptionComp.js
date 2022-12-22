acbce0b068e35f6017e1d054a21ec3af2b6c78ad
Anmol Kaushik <73958690+anmol-ui@users.noreply.github.com>
2022-12-22 12:33:52 +0530

    Update DescriptionComp.js

--- src/components/DescriptionComp.js
+++ src/components/DescriptionComp.js
@@ -3,7 +3,7 @@ import React from "react";
 function DescriptionComp() {
   return (
     <div class="description">
-        <p>“Analytics, engagement, and intelligence are what we do. The conversions are yours to discover.”</p>
+        <p>“Email has an ability many channels don't: creating valuable, personal touches - at scale.” - David Newman</p>
     </div>
   );
 }
